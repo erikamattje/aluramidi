93a38aea9c7cf72dea36bf39ed8ec092f48addc3
Erika Mattje <erikamattje@gmail.com>
2022-11-17 23:45:26 -0300

    :up: Atualizando função tocaSom

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play(); //selecionando um objeto de id som_tecla_pom dentro do document. O ponto após acessa as funcionalidades do resultado dessa busca do querySelector
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play(); //selecionando um objeto de id som_tecla_pom dentro do document. O ponto após acessa as funcionalidades do resultado dessa busca do querySelector
 }
 
 //selecionando um objeto de classe tecla_pom dentro do document
 //Atribui o som no clique, não tem os parenteses da função pra não executar automaticamente quando a página carrega.
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom; 
 
+/*function tocaSom(nome){
+    document.querySelector(`'#som_tecla_'${nome}`).play();
+}
 
-document.querySelectorAll('.tecla')
+document.querySelector('.tecla_clap').onclick = tocaSom(clap);
+*/
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); //seleciona as classes 'tecla' 
+
+let i = 0;
+
+while ( i< listaDeTeclas.length ) {
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`; //template string
+
+    
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+    i++;
+
+    console.log(i); 
+}
 
